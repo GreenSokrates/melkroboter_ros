477158235ab63047fd9c9979df48429aa144ed7d
GreenSokrates <meierluis@hotmail.de>
2018-07-01 19:16:24 +0200

    RobterNode to ask to move the robot

--- src/core/include/core/RoboterNode.h
+++ src/core/include/core/RoboterNode.h
@@ -16,6 +16,7 @@ class RoboterNode
 public:
   RoboterNode(ros::NodeHandle *nodehandle);
   virtual ~RoboterNode(){};
+  bool moveToNamed(std::string poseName);
 
 private:
   ros::NodeHandle nh_;
@@ -25,8 +26,9 @@ private:
 
   void initializeServices();
   void initializePlanner();
-  bool MoveToPose_cb_(core::moveToPose::Request &req, core::moveToPose::Response &res);
-  bool MoveLinear_cb_(core::moveLinear::Request &req, core::moveLinear::Response &res);
+  bool moveToPose_cb_(core::moveToPose::Request &req, core::moveToPose::Response &res);
+  bool moveLinear_cb_(core::moveLinear::Request &req, core::moveLinear::Response &res);
+
 };
 
 #endif /* ROBOTER_NODE_H_ */
--- src/core/src/RoboterNode.cpp
+++ src/core/src/RoboterNode.cpp
@@ -1,22 +1,25 @@
 #include "core/RoboterNode.h"
 
-RoboterNode::RoboterNode(ros::NodeHandle *nodehandle) : nh_(*nodehandle) {
+RoboterNode::RoboterNode(ros::NodeHandle *nodehandle) : nh_(*nodehandle)
+{
   ROS_INFO("Constructed a RoboterNodeClass");
   initializeServices();
   initializePlanner();
 }
 
-void RoboterNode::initializeServices() {
+void RoboterNode::initializeServices()
+{
   ROS_INFO("Initializing Services");
   MoveToPose_Service_ =
       nh_.advertiseService("/Melkroboter/MoveToPose",
-                           &RoboterNode::RoboterNode::MoveToPose_cb_, this);
+                           &RoboterNode::RoboterNode::moveToPose_cb_, this);
   MoveLinear_Service_ =
       nh_.advertiseService("/Melkroboter/MoveLinear",
-                           &RoboterNode::RoboterNode::MoveLinear_cb_, this);
+                           &RoboterNode::RoboterNode::moveLinear_cb_, this);
 }
 
-void RoboterNode::initializePlanner() {
+void RoboterNode::initializePlanner()
+{
   ROS_INFO("Setting up Planner");
   // Define MoveGroup and PlanningSceneInterface
   group.reset(
@@ -30,8 +33,9 @@ void RoboterNode::initializePlanner() {
   group->setMaxVelocityScalingFactor(0.1);
 }
 
-bool RoboterNode::MoveToPose_cb_(core::moveToPose::Request &req,
-                                 core::moveToPose::Response &res) {
+bool RoboterNode::moveToPose_cb_(core::moveToPose::Request &req,
+                                 core::moveToPose::Response &res)
+{
   ROS_INFO("Moving to Pose");
   geometry_msgs::Pose myPose;
   myPose.position.x = req.x;
@@ -41,28 +45,67 @@ bool RoboterNode::MoveToPose_cb_(core::moveToPose::Request &req,
   myPose.orientation.x = req.oX;
   myPose.orientation.y = req.oY;
   myPose.orientation.z = req.oZ;
-  group->setNamedTarget("search_ur");
-  if (group->move()) {
+  group->setPoseTarget(myPose);
+  if (group->move())
+  {
     res.status = true;
-  } else {
+  }
+  else
+  {
     res.status = false;
   }
   return true;
 }
 
-bool RoboterNode::MoveLinear_cb_(core::moveLinear::Request &req,
-                                 core::moveLinear::Response &res) {
+bool RoboterNode::moveToNamed(std::string poseName)
+{
+  group->setNamedTarget(poseName);
+  if (group->move())
+  {
+    //res.status = true;
+  }
+  else
+  {
+    //res.status = false;
+  }
+  return true;
+}
+
+bool RoboterNode::moveLinear_cb_(core::moveLinear::Request &req,
+                                 core::moveLinear::Response &res)
+{
   ROS_INFO("Moving Linear");
   return true;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   ros::init(argc, argv, "Melkroboter_RoboterNode");
   ros::NodeHandle nh;
 
   RoboterNode roboternode(&nh);
 
-  ROS_INFO("RoboterNode is up");
-  ros::spin();
+  ros::Duration(5).sleep(); // sleep for half a second
+
+  std::cout << "Roboter is ready to start!  \n\n"
+            << "It will move to Home first\n"
+            << "Ready and all clear? (y/n)" << std::endl;
+  char response;
+  std::cin >> response;
+  if (response != 'y' && response != 'Y')
+  {
+    ROS_ERROR("Make sure all is clear!");
+    ros::Duration(5).sleep();
+    ros::shutdown();
+    return -1;
+  }
+
+      roboternode.moveToNamed("home_ur");
+
+    //"search_ur"
+
+    ROS_INFO("RoboterNode is up");
+    ros::spin();
+
   return 0;
 }
